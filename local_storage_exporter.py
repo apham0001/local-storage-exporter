543e6478e0f3e0475f38908b454d2d5d803b3cd3
Korel <reyan-korel.erben@prokube.ai>
2024-12-17 11:26:37 +0100

    Changed hardcoded volumeMount mountPath and volume hostPath to searching and pairing them using the name through k8s api. Additional refactoring during implementation.

--- local_storage_exporter.py
+++ local_storage_exporter.py
@@ -5,10 +5,14 @@ import subprocess
 import time
 import re
 import os
+from pathlib import Path
 
 from kubernetes import client, config
 from prometheus_client import Gauge, start_http_server
 from kubernetes.client.models.v1_persistent_volume_list import V1PersistentVolumeList
+from kubernetes.client.models.v1_persistent_volume import V1PersistentVolume
+from kubernetes.client.models.v1_pod_list import V1PodList
+from kubernetes.client.models.v1_pod import V1Pod
 
 # Set up logging and create handler for info logs
 handler = logging.StreamHandler()
@@ -18,14 +22,6 @@ handler.setFormatter(formatter)
 _logger = logging.getLogger(__name__)
 _logger.setLevel(logging.INFO)
 _logger.addHandler(handler)
-
-
-@dataclass
-class Volume:
-    pvc_name: str
-    pv_name: str
-    storage_path: str
-    storage_capacity: str
 
 
 # fmt: off
@@ -51,8 +47,14 @@ class LocalStorageExporter:
     gauge: Gauge
     k8s_client: client.CoreV1Api
     storage_class_name: str
+    hostpath_mount_paths: list[Path]
 
-    def __init__(self, storage_class_name: str, incluster: bool = True, config_file: str | None = None, ):
+    def __init__(
+        self,
+        storage_class_name: str,
+        incluster: bool = True,
+        config_file: str | None = None,
+    ):
         try:
             if incluster:
                 config.load_incluster_config()
@@ -65,35 +67,76 @@ class LocalStorageExporter:
             _logger.error(f"Failed to load k8s config: {e}")
             raise
 
+        self.hostpath_mount_paths = self.find_hostpath_mount_paths()
+        if len(self.hostpath_mount_paths) == 0:
+            _logger.error("Failed to find any hostpath mount path")
+            raise Exception("Failed to find any hostpath mount path")
+
         self.gauge = Gauge(
             name="local_storage_pv_used_bytes",
             documentation="The amount of bytes used by local storage volume",
-            labelnames=["pvc_name", "pv_name", "storage_path", "storage_capacity", "storage_class_name"],
+            labelnames=[
+                "pvc_name",
+                "pv_name",
+                "storage_path",
+                "storage_capacity",
+                "storage_class_name",
+            ],
         )
         self.storage_class_name = storage_class_name
 
-    def get_volumes(self) -> list[Volume]:
-        volumes = []
-        pvs: V1PersistentVolumeList = self.k8s_client.list_persistent_volume()
-        for pv in pvs.items:
-            if pv.spec.storage_class_name == self.storage_class_name:
-                volumes.append(
-                    Volume(
-                        pvc_name=pv.spec.claim_ref.name,
-                        pv_name=pv.metadata.name,
-                        storage_path=pv.spec.local.path,
-                        storage_capacity=convert_storage_capacity_to_bytes(
-                            pv.spec.capacity["storage"]
-                        ),
-                    )
-                )
-        return volumes
+    def get_pod(self) -> V1Pod:
+        pod_hostname = os.getenv("HOSTNAME")
+        with open(
+            "/var/run/secrets/kubernetes.io/serviceaccount/namespace", "r"
+        ) as file:
+            pod_namespace = file.read().strip()
 
-    @staticmethod
-    def get_volume_usage(volume: Volume) -> int | None:
+        pods: V1PodList = self.k8s_client.list_namespaced_pod(
+            namespace=pod_namespace, field_selector=f"metadata.name={pod_hostname}"
+        )
+        assert (
+            len(pods.items) == 1
+        ), f"Expected to find one pod with name {pod_hostname} in namespace {pod_namespace}, but found {len(pods.items)}"
+
+        pod: V1Pod = pods.items[0]
+        return pod
+
+    def find_hostpath_mount_paths(self) -> dict[Path, Path]:
+        pod = self.get_pod()
+        assert len(pod.spec.containers) == 1, "Expected to find one container in pod"
+        container = pod.spec.containers[0]
+        mount_paths = {}
+        for volume in pod.spec.volumes:
+            if volume.host_path:
+                for volume_mount in container.volume_mounts:
+                    if volume_mount.name == volume.name:
+                        mount_paths[Path(volume.host_path.path)] = Path(
+                            volume_mount.mount_path)
+                        break
+        return mount_paths
+
+    def get_pvs(self) -> V1PersistentVolumeList:
+        pvs: V1PersistentVolumeList = self.k8s_client.list_persistent_volume()
+        pvs.items = [pv for pv in pvs.items if pv.spec.storage_class_name == self.storage_class_name] 
+        return pvs
+
+    def get_pv_usage(self, pv: V1PersistentVolume) -> int | None:
+        base_name = os.path.basename(pv.spec.local.path)
+        dir_name = os.path.dirname(pv.spec.local.path)
+
+        path: Path = self.hostpath_mount_paths.get(Path(dir_name))
+        if path is None:
+            _logger.error(f"Failed to find hostpath mount path for {dir_name}")
+            return None
+        path = path / base_name    
+        if not path.exists():
+            _logger.error(f"Path {path} does not exist")
+            return None
+        
         try:
             result = result = subprocess.run(
-                ["du", "-sb", f"/volumes/{volume.pv_name}"],
+                ["du", "-sb", os.fspath(path)],
                 capture_output=True,
                 text=True,
                 check=True,
@@ -101,42 +144,41 @@ class LocalStorageExporter:
             size = result.stdout.split("\t")[0]
             return int(size)
         except Exception as e:
-            _logger.error(f"Failed to get volume usage for {volume.storage_path}: {e}")
+            _logger.error(f"Failed to get volume usage for {path}: {e}")
             return None
 
     def update_metrics(self):
-        volumes = self.get_volumes()
-        for volume in volumes:
-            usage = LocalStorageExporter.get_volume_usage(volume)
+        pvs = self.get_pvs()
+        for pv in pvs.items:
+            usage = self.get_pv_usage(pv)
+            gauge = self.gauge.labels(
+                pv.spec.claim_ref.name,
+                pv.metadata.name,
+                pv.spec.local.path,
+                convert_storage_capacity_to_bytes(pv.spec.capacity["storage"]),
+                self.storage_class_name,
+            )
             if usage is not None:
-                self.gauge.labels(
-                    volume.pvc_name,
-                    volume.pv_name,
-                    volume.storage_path,
-                    volume.storage_capacity,
-                    self.storage_class_name
-                ).set(usage)
+                gauge.set(usage)
             else:
-                self.gauge.labels(
-                    volume.pvc_name,
-                    volume.pv_name,
-                    volume.storage_path,
-                    volume.storage_capacity,
-                    self.storage_class_name
-                ).set(-1.0)
+                gauge.set(-1)
 
 
 def main():
-    storage_class_name = os.environ.get("STORAGE_CLASS_NAME")
-    port = 9100
-    _logger.info(f"Given Storageclass name: {storage_class_name}")
-    
-    lse = LocalStorageExporter(storage_class_name=storage_class_name)    
-    start_http_server(port)
-    _logger.info(f"Started local storage exporter on port {port}")
-    while True:
-        lse.update_metrics()
-        time.sleep(60)
+    try:
+        storage_class_name = os.environ.get("STORAGE_CLASS_NAME")
+        port = 9100
+        _logger.info(f"Given Storageclass name: {storage_class_name}")
+
+        lse = LocalStorageExporter(storage_class_name=storage_class_name)
+        start_http_server(port)
+        _logger.info(f"Started local storage exporter on port {port}")
+        while True:
+            lse.update_metrics()
+            time.sleep(30)
+    except Exception as e:
+        _logger.error(f"Caught exception in main: {e}")
+        raise
 
 
 if __name__ == "__main__":
